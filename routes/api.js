ec13af948bc6d8729aff0444f1a6dffd4e3d4690
joeydash <joeydash>
2018-03-20 10:27:50 +0530

    feat: view option added

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,7 @@ router.get('/read/:id/:skip/:limit', function(req, res, next) {
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
             var dbo = db.db(db_name);
-            dbo.collection(req.params.id).find({}).skip(req.params.skip).limit(req.params.skip)toArray(function(error, result) {
+            dbo.collection(req.params.id).find({}).skip(req.params.skip).limit(req.params.skip).toArray(function(error, result) {
                 if (error) {res.json(error);}
                 else{
                     res.json(result);
