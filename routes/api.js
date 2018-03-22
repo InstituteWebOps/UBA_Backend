b4520197bbac6c01e5f6e97b51497f86b0289f63
joeydash <joeydash>
2018-03-22 22:12:46 +0530

    feat: view option added

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://joeydash:joeydash@ds135790.mlab.com:35790/joeydash";
-var db_name = "joeydash";
+// var url = "mongodb://joeydash:joeydash@ds135790.mlab.com:35790/joeydash";
+var url = "mongodb://localhost:27017/";
+var db_name = "UBA_DB";
+// var db_name = "joeydash";
 var collection_list = ["data"];
 var contains = function(needle) {
     var findNaN = needle !== needle;
